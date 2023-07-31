7630fddd57e0a80ad499942bb757a99bde79c222
ElJakes <jidejacobs1@gmail.com>
2023-07-31 16:55:55 +0100

    build and style the app, write nested for of loop

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+import catsData from './catsData.js'
+
+const getEmotionsArray = (cats) => {
+    for (let cat of cats) {
+        for (let emotion of cat.emotionTags) {
+            console.log(emotion)
+        }
+    }
+}
+
+getEmotionsArray(catsData)
