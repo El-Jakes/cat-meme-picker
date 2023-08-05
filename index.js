3ac21e62b3b482dfda560d2036e79aac656ca7be
ElJakes <jidejacobs1@gmail.com>
2023-08-05 18:04:02 +0100

    add accent color to the highlight class, target the input and label and add the highlight class to them

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ import { catsData } from "./catsData.js";
 
 const emotionsRadios = document.querySelector("#emotion-radios");
 
-emotionsRadios.addEventListener("change", (e) => {
-    console.log(document.getElementById(e.target.id))
-})
+
+const hightCheckedOption = (e) => {
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+emotionsRadios.addEventListener("change", hightCheckedOption)
 
 const getEmotionsArray = (cats) => {
   const emotionsArray = [];
   for (let cat of cats) {
     for (let emotion of cat.emotionTags) {
-        if (!emotionsArray.includes(emotion)) {
-            emotionsArray.push(emotion);
-        } 
+        emotionsArray.includes(emotion) ? '' : emotionsArray.push(emotion)
     }
   }
   return emotionsArray;
@@ -24,13 +24,13 @@ const renderEmotionRadios = (cats) => {
   for (let emotion of emotions) {
     renderedEmotions += `
         <div class="radio">
+        <label for="${emotion}">${emotion}</label>
         <input 
         type="radio" 
         id="${emotion}" 
         value="${emotion}"
         name="emotion-radios"
         >
-        <label for="${emotion}">${emotion}</label>
         </div>
         `;
   }
